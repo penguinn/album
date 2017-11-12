abf846a7ad2b2c296f6b2239a0cc8b2c84d45bee
penguinn <852198764@qq.com>
2017-11-12 23:27:02 +0800

    modify vendor

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
+	"github.com/penguinn/album/controllers"
+	"github.com/penguinn/penguin/component/config"
+	"github.com/penguinn/penguin/component/db"
+	"github.com/penguinn/penguin/component/log"
 	"github.com/penguinn/penguin/component/router"
 	"github.com/penguinn/penguin/component/server"
-	"github.com/penguinn/album/controllers"
 	"github.com/penguinn/penguin/component/session"
 )
 
 func main() {
+	flag.Parse()
+
+	server.Use(config.ConfigComponent{})
+	server.Use(log.LogComponent{})
+	server.Use(router.RouterComponent{})
+	server.Use(db.DBComponent{})
+
 	router.RegisterController(controllers.NewBaseController(), session.Middleware)
 	router.RegisterControllerGroup(controllers.NewCheckController(), "api", session.Middleware)
 	router.RegisterControllerGroup(controllers.NewUserController(), "api", session.Middleware)
