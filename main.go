d1d1a276e1c59a12b92613e12dc110ec74502f05
penguinn <852198764@qq.com>
2017-11-12 23:43:23 +0800

    modify import 空不空行是有区别的

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"flag"
+	_ "github.com/penguinn/album/init"
+
 	"github.com/penguinn/album/controllers"
-	"github.com/penguinn/penguin/component/config"
-	"github.com/penguinn/penguin/component/db"
-	"github.com/penguinn/penguin/component/log"
 	"github.com/penguinn/penguin/component/router"
 	"github.com/penguinn/penguin/component/server"
 	"github.com/penguinn/penguin/component/session"
 )
 
 func main() {
-	flag.Parse()
-
-	server.Use(config.ConfigComponent{})
-	server.Use(log.LogComponent{})
-	server.Use(router.RouterComponent{})
-	server.Use(db.DBComponent{})
 
 	router.RegisterController(controllers.NewBaseController(), session.Middleware)
 	router.RegisterControllerGroup(controllers.NewCheckController(), "api", session.Middleware)
